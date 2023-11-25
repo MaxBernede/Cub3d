d9b139049b5f28a420165776ac0a12f85e9a2e75
MaxBernede <bernede.maximilien@gmail.com>
2023-11-25 19:25:09 +0100

    first push, i kept lot of files to change them accordingly to cub3d

---
+++ src/camera.c
@@ -0,0 +1,6 @@
+#include "cub3d.h"
+
+void normalize_camera(t_param *param)
+{
+	
+}
---
+++ src/utils/fill_to_null.c
@@ -0,0 +1,7 @@
+#include "cub3d.h"
+
+void	init_null(t_param *param)
+{
+	param->lights = NULL;
+	param->shapes = NULL;
+}
