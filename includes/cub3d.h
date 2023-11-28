e7c337a54cbd3724a92d112a08350a85384dd15d
MaxBernede <bernede.maximilien@gmail.com>
2023-11-28 02:12:29 +0100

    .h files norminette fixed as well

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -1,93 +1,92 @@
 #ifndef CUB3D_H
-#define CUB3D_H
+# define CUB3D_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include "struct.h"
-#include "../libft/libft.h"
-#include "../MLX42/include/MLX42/MLX42.h"
+# include "../MLX42/include/MLX42/MLX42.h"
+# include "../libft/libft.h"
+# include "struct.h"
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
 
-#define OK 0
-#define ERROR 1
+# define OK 0
+# define ERROR 1
 
-//Run errors
-#define ERR_START_POS "Position not found\n"
-#define ERR_CHARACTER "Character found 0 or more than 1 time in the file\n"
-#define ERR_ARG "Error: Not enough arguments to run\nShould be ./cub3d 'file.cub'\n"
-#define ERR_FILE "Error: File given doesn't exist or no rights to access it\n"
-#define ERR_NORT "Error: File doesn't end with .rt, cannot be accepted\n"
-#define ERR_FILE_DATA "Error: File datas are not correct, exit\n"
+// Run errors
+# define ERR_START_POS "Position not found\n"
+# define ERR_CHARACTER "Character found 0 or more than 1 time in the file\n"
+# define ERR_ARG "Error: Not enough arguments\nShould be ./cub3d 'file.cub'\n"
+# define ERR_FILE "Error: File given doesn't exist or no rights to access it\n"
+# define ERR_NORT "Error: File doesn't end with .rt, cannot be accepted\n"
+# define ERR_FILE_DATA "Error: File datas are not correct, exit\n"
 
-//Errors globals
-#define ERR_MLC "Error: Malloc Failed\n"
+// Errors globals
+# define ERR_MLC "Error: Malloc Failed\n"
 
-//Errors fills
-#define ERR_FILL_COLORS "Error\n Error fill colors\n"
-#define ERR_FILL_MAP "Error\nMap fill error\n"
-#define ERR_OPEN_TEXTURES "Error\nCannot open textures\n"
-#define ERR_TEXTURES "Error\nTextures sides error\n"
-#define ERR_FLOOR_CEILING "Error\nFloor and ceiling error\n"
+// Errors fills
+# define ERR_FILL_COLORS "Error\n Error fill colors\n"
+# define ERR_FILL_MAP "Error\nMap fill error\n"
+# define ERR_OPEN_TEXTURES "Error\nCannot open textures\n"
+# define ERR_TEXTURES "Error\nTextures sides error\n"
+# define ERR_FLOOR_CEILING "Error\nFloor and ceiling error\n"
 
-//Sides Error
-#define ERR_NO "Error\nError North\n"
-#define ERR_SO "Error\nError South\n"
-#define ERR_EA "Error\nError East\n"
-#define ERR_WE "Error\nError West\n"
+// Sides Error
+# define ERR_NO "Error\nError North\n"
+# define ERR_SO "Error\nError South\n"
+# define ERR_EA "Error\nError East\n"
+# define ERR_WE "Error\nError West\n"
 
-//check_map
-int check_map(t_param *param);
+// check_map
+int			check_map(t_param *param);
 
-//check_line.c
-int	get_str_without_nl(char *line, char **sub);
+// check_line.c
+int			get_str_without_nl(char *line, char **sub);
 
-//parsing.c
-int	fill_datas(char *arg, t_param *param);
+// parsing.c
+int			fill_datas(char *arg, t_param *param);
 
-//!MLX
-void my_hook(void* param);
+//! MLX
+void		my_hook(void *param);
 
-//!START
-int start(t_param *param, t_window w);
+//! START
+int			start(t_param *param, t_window w);
 
-t_shapes *create_shape_node();
-t_shapes *insert_shape_start(t_shapes *head, t_shapes *new_light);
-t_shapes *insert_shape_end(t_shapes *head, t_shapes *new_light);
-void free_shapes_list(t_shapes *head);
-void print_shapes(t_shapes *head);
-void print_flood(t_map map);
+t_shapes	*create_shape_node(void);
+t_shapes	*insert_shape_start(t_shapes *head, t_shapes *new_light);
+t_shapes	*insert_shape_end(t_shapes *head, t_shapes *new_light);
+void		free_shapes_list(t_shapes *head);
+void		print_shapes(t_shapes *head);
+void		print_flood(t_map map);
 
-//!UTILS
-//clean.c
-void clean_everything(t_param *param);
+//! UTILS
+// clean.c
+void		clean_everything(t_param *param);
 
-//ft_atof.c
-double ft_atof(char *nptr);
+// ft_atof.c
+double		ft_atof(char *nptr);
 
-//to.c
-int	to_decimal(char *s, double *f);
+// to.c
+int			to_decimal(char *s, double *f);
 
-int is_scale(char *s, double *f);
-int	is_colors(char *s, int *r, int *g, int *b);
+int			is_scale(char *s, double *f);
+int			is_colors(char *s, int *r, int *g, int *b);
 
-void	ft_2d_print(char **str);
-int	cmp(char *s1, char *s2);
-int nb_or_comma(char *s);
+void		ft_2d_print(char **str);
+int			cmp(char *s1, char *s2);
+int			nb_or_comma(char *s);
 
-//valid_input.c
-bool	check_file(char *arg);
-int		open_file(char *arg);
+// valid_input.c
+bool		check_file(char *arg);
+int			open_file(char *arg);
 
-//map_parse
-char	**get_map(t_param *p);
-int		parse_map(char *line, t_param *param);
+// map_parse
+char		**get_map(t_param *p);
+int			parse_map(char *line, t_param *param);
 
-//nodes
-void	print_list(t_node *head);
-void	ft_free_lst(t_node *head);
-void	ft_free_node(t_node *node);
-int		insert_node(t_node **head, char *line);
-int		ft_our_lst_size(t_node *lst);
-
+// nodes
+void		print_list(t_node *head);
+void		ft_free_lst(t_node *head);
+void		ft_free_node(t_node *node);
+int			insert_node(t_node **head, char *line);
+int			ft_our_lst_size(t_node *lst);
 
 #endif
--- includes/struct.h
+++ includes/struct.h
@@ -1,82 +1,80 @@
 #ifndef STRUCT_H
-#define STRUCT_H
+# define STRUCT_H
 
-#include "../MLX42/include/MLX42/MLX42.h"
+# include "../MLX42/include/MLX42/MLX42.h"
 
 typedef struct s_color
 {
-	int r;
-	int g;
-	int b;
-}	t_color;
+	int				r;
+	int				g;
+	int				b;
+}					t_color;
 
 typedef struct s_vec3
 {
-	double x;
-	double y;
-	double z;
-}	t_vec3;
+	double			x;
+	double			y;
+	double			z;
+}					t_vec3;
 
 typedef struct s_window
 {
-	int width;
-	int height;
-}	t_window;
+	int				width;
+	int				height;
+}					t_window;
 typedef struct s_lkd_list
 {
-	double scale;
-	t_vec3 p;
-	t_color color;
-	struct s_list *next;
-}	t_lkd_list;
+	double			scale;
+	t_vec3			p;
+	t_color			color;
+	struct s_list	*next;
+}					t_lkd_list;
 
 typedef struct s_ray
 {
-	t_vec3	origin;
-	t_vec3	direction;
-}	t_ray;
+	t_vec3			origin;
+	t_vec3			direction;
+}					t_ray;
 
 typedef struct s_shapes
 {
-	t_vec3 p;
-	t_vec3 vec;
-	t_color color;
-	struct s_shapes *next;
-}	t_shapes;
+	t_vec3			p;
+	t_vec3			vec;
+	t_color			color;
+	struct s_shapes	*next;
+}					t_shapes;
 
 typedef struct s_map
 {
-	char	**flood;
-	char	**map;
-	int		length;
-	int		height;
-}	t_map;
+	char			**flood;
+	char			**map;
+	int				length;
+	int				height;
+}					t_map;
 
 typedef struct s_node
 {
 	char			*line;
 	struct s_node	*next;
-}	t_node;
-
+}					t_node;
 
 typedef struct s_param
 {
 	t_color			floor;
 	t_color			ceiling;
-	mlx_texture_t*	north;
-	mlx_texture_t*	south;
-	mlx_texture_t*	west;
-	mlx_texture_t*	east;
+	mlx_texture_t	*north;
+	mlx_texture_t	*south;
+	mlx_texture_t	*west;
+	mlx_texture_t	*east;
 	int				fd;
 	bool			end_map_parse;
 	t_node			*tmp_map;
 	t_map			map;
 
-	mlx_t		*mlx;
-	mlx_image_t	*img;
-	t_window	window;
-	t_shapes	*shapes;
-}	t_param;
-
+	mlx_t			*mlx;
+	mlx_image_t		*img;
+	t_window		window;
+	t_shapes		*shapes;
+}					t_param;
 
 #endif
